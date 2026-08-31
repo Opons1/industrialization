c0c14c54e8ce19506841dff84bbf81f3944d7e19
Opons1 <98196027+Opons1@users.noreply.github.com>
2026-08-31 14:01:02 -0700

    add some groups

--- mods/industrialization_core/hand.lua
+++ mods/industrialization_core/hand.lua
@@ -4,7 +4,7 @@ core.override_item("", {
 		max_drop_level = 0,
 		groupcaps = {
 			crumbly = {times={[1] = 5}, maxlevel = 1, uses = 0},
-			snappy = {times={[1] = 3}, maxlevel = 1, uses = 0},
+			snappy = {times={[1] = 1}, maxlevel = 1, uses = 0},
 			oddly_breakable_by_hand = {times={ [1] = 0,[2] = 0.5, [3] = 1, [4] = 2, [5] = 4, [6] = 8}, uses=0}
 		},
 		damage_groups = {fleshy=1},
--- mods/industrialization_mapgen/nodes.lua
+++ mods/industrialization_mapgen/nodes.lua
@@ -1,10 +1,12 @@
 core.register_node(":industrialization:stone", {
-    tiles = {"industrialization_stone.png"}
+    tiles = {"industrialization_stone.png"},
+    groups = {stone = 1}
 })
 core.register_alias("mapgen_stone", "core:stone")
 
 core.register_node(":industrialization:dirt", {
-    tiles = {"industrialization_dirt.png"}
+    tiles = {"industrialization_dirt.png"},
+    groups = {crumbly = 1, soil = 1}
 })
 
 core.register_node(":industrialization:grass", {
@@ -12,14 +14,17 @@ core.register_node(":industrialization:grass", {
     "industrialization_grass_top.png",
     "industrialization_dirt.png",
 	"industrialization_dirt.png^industrialization_grass_side.png",
-    }
+    },
+    groups = {crumbly = 1, soil = 1, grass_block = 1}
 })
 
 core.register_node(":industrialization:oak_tree", {
 	tiles = {"industrialization_oak_top.png", "industrialization_oak_top.png", "industrialization_oak_tree.png"},
+    groups = {tree = 1}
 })
 
 core.register_node(":industrialization:leaves", {
     drawtype = "allfaces",
-    tiles = {"industrialization_oak_leaves.png"}
+    tiles = {"industrialization_oak_leaves.png"},
+    groups = {leaves = 1, snappy = 1}
 })
